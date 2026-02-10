bbd50881373b03e06b992f65595d9e3691cab8ee
Твоё Имя <dorovskoy567@mail.ru>
2026-02-10 23:34:34 +0300

    Добавлено управление профилями и интеграция с API, обновлены зависимости и конфигурации

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
 import { ProfileCard } from './common-ui/profile-card/profile-card';
+import { ProfileService } from './data/services/profile';
+import { Profile } from './data/services/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  standalone: true,
+  imports: [ProfileCard],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('tik-talk');
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
 }
--- src/app/common-ui/profile-card/profile-card.ts
+++ src/app/common-ui/profile-card/profile-card.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/services/interfaces/profile.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.scss',
 })
 export class ProfileCard {
-
+  @Input() profile!: Profile;
 }
