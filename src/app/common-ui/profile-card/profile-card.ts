b1d177283e0ec171b457590f424aca9a4a3eb7ca
Твоё Имя <dorovskoy567@mail.ru>
2026-02-16 23:40:57 +0300

    refactor: restructure app to use routing and remove profile card display from app component
    feat: add login, profile, and search pages with corresponding routes
    
    fix: update profile card to use dynamic avatar URLs with imgUrl pipe
    
    style: adjust profile card layout and add styles for profile card tags
    
    feat: implement search page to display profiles using profile card component
    
    chore: add global box-sizing and reset styles for better layout consistency

--- src/app/common-ui/profile-card/profile-card.ts
+++ src/app/common-ui/profile-card/profile-card.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Profile } from '../../data/services/interfaces/profile.interface';
 import { CommonModule } from '@angular/common';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url-pipe';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [CommonModule],
+  imports: [CommonModule, ImgUrlPipe],
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.scss',
 })
---
+++ src/app/pages/search-page/search-page.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { Profile } from '../../data/services/interfaces/profile.interface';
+import { ProfileService } from '../../data/services/profile';
+import { ProfileCard } from '../../common-ui/profile-card/profile-card';
+
+@Component({
+  selector: 'app-search-page',
+  imports: [ProfileCard],
+  templateUrl: './search-page.html',
+  styleUrl: './search-page.scss',
+})
+export class SearchPage {
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
+}
