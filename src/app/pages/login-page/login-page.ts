7e025509ab7f80cf7a8989823e2b71aa7bd8fff1
Твоё Имя <dorovskoy567@mail.ru>
2026-04-12 14:15:42 +0300

    feat: add logo SVG, authentication service, and layout components
    - Added a new SVG logo file at `public/assets/svg/logo-big.svg`.
    - Implemented an authentication service in `src/app/auth/auth.ts` for user login.
    - Created a layout component with sidebar in `src/app/common-ui/layout/` including HTML, SCSS, and TypeScript files.
    - Developed a sidebar component in `src/app/common-ui/sidebar/` with basic structure.
    - Introduced global margin utility classes in `src/margins.scss`.

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../../auth/auth';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
+  authService = inject(Auth);
 
+  form = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+    if (this.form.valid) {
+      //@ts-ignore
+      this.authService.login(this.form.value);
+    }
+  }
 }
