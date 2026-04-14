d5f49b5ef51a7abc585ec4923202c58f258529ad
Твоё Имя <dorovskoy567@mail.ru>
2026-04-14 17:10:30 +0300

    refactor: изменить метод входа для использования FormData и улучшить обработку формы на странице входа

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Auth } from '../../auth/auth';
+import { from, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
@@ -16,11 +17,21 @@ export class LoginPage {
     password: new FormControl(null, Validators.required),
   });
 
+  constructor() {
+    from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      .pipe(
+        map((val) => val * 2),
+        take(2),
+      )
+      .subscribe((val) => {
+        console.log(val);
+      });
+  }
+
   onSubmit() {
-    console.log(this.form.value);
     if (this.form.valid) {
       //@ts-ignore
-      this.authService.login(this.form.value);
+      this.authService.login(this.form.value).subscribe();
     }
   }
 }
