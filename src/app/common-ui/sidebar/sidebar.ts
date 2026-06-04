11be0c8ab9d26eb9011ada4dec33d134385a9879
Твоё Имя <dorovskoy567@mail.ru>
2026-06-04 18:33:55 +0300

    feat: добавить боковую панель с динамическим меню и карточками подписчиков

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 import { SvgIcon } from '../svg-icon/svg-icon';
+import { NgFor } from '@angular/common';
+import { SubscriberCard } from './subscriber-card/subscriber-card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIcon],
+  imports: [SvgIcon, NgFor, SubscriberCard, RouterLink],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
-export class Sidebar {}
+export class Sidebar {
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: 'chats',
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search',
+    },
+  ];
+}
